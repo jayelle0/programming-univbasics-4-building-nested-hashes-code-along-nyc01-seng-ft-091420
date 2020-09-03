d2e658961fde59136a698e197d2b8b442987c213
Jacqueline <lojacqueline2@gmail.com>
2020-09-03 00:45:49 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -26,12 +26,12 @@ def third_challenge
         :name => "Lady Capulet", 
         :age  =>  "51"
       },
-        },
+      
       :heroine => {
         :name => "Juliet", 
         :age  =>  "15", 
         :status => "alive"
-      },
+    
         },
       :heroine_friends => []
    }
